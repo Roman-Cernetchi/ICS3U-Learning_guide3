bb9d28c912089b7ea120cda76dc861516caad429
Roman-Cernetchi <74559770+Roman-Cernetchi@users.noreply.github.com>
2021-01-26 12:06:56 -0500

    Add files via upload

---
+++ Step 6/constants (1).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by: Roman Cernetchi
+# Created on: Janurary 2021
+# This is a constants program on Pybadge
+
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ROCKS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+# Using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
+
+# new pallet for red filled text
+BLUE_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+               b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
